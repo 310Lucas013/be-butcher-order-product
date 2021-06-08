34e991355a6f0d3fc13feda19966a264ef66ff3c
Luuk Vermeer <l.vermeer@student.fontys.nl>
2021-06-08 16:05:55 +0200

    added the hibernate implementation so it will not give stackoverflow errors

--- src/main/java/com/lucas/product/controllers/ProductController.java
+++ src/main/java/com/lucas/product/controllers/ProductController.java
@@ -1,11 +1,21 @@
 package com.lucas.product.controllers;
 
+import com.google.gson.ExclusionStrategy;
+import com.google.gson.FieldAttributes;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.lucas.product.models.HibernateProxyTypeAdapter;
+import com.lucas.product.models.Product;
 import com.lucas.product.models.dto.ProductDto;
 import com.lucas.product.services.ProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/products")
@@ -18,16 +28,52 @@ public class ProductController {
 
     @PostMapping
     public ResponseEntity<?> createProduct(@RequestBody ProductDto productDto) {
-        return new ResponseEntity<>(productService.createProduct(productDto), HttpStatus.CREATED);
+        Gson gson = initiateGson();
+        Product product = productService.createProduct(productDto);
+        String result = gson.toJson(product);
+        return new ResponseEntity<>(result, HttpStatus.CREATED);
     }
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<?> getProductById(@PathVariable("id") Long id) {
-        return new ResponseEntity<>(productService.getProductById(id), HttpStatus.OK);
+        Gson gson = initiateGson();
+        Product product = productService.getProductById(id);
+        String result = gson.toJson(product);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @GetMapping
     public ResponseEntity<?> getProducts() {
-        return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.OK);
+        Gson gson = initiateGson();
+        List<Product> products = productService.getAllProducts();
+        String result = gson.toJson(products);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
+
+    private Gson initiateGson() {
+        GsonBuilder b = new GsonBuilder();
+        b.registerTypeAdapterFactory(HibernateProxyTypeAdapter.FACTORY)
+                .excludeFieldsWithModifiers(Modifier.TRANSIENT)
+                .setExclusionStrategies(new ExclusionStrategy() {
+                    @Override
+                    public boolean shouldSkipField(FieldAttributes f) {
+                        boolean exclude = false;
+                        try {
+                            exclude = EXCLUDE.contains(f.getName());
+                        } catch (Exception ignore) {
+                        }
+                        return exclude;
+                    }
+
+                    @Override
+                    public boolean shouldSkipClass(Class<?> clazz) {
+                        return false;
+                    }
+                });
+        return b.create();
+    }
+
+    private static final List<String> EXCLUDE = new ArrayList<>() {{
+
+    }};
 }
